a1eb9a609cffa3ed1cf7a1037bd7f00bf98a8906
N_Sivkov <n_sivkov@kbtu.kz>
2025-01-23 11:57:26 +0500

    w3school examples from Python Strings

---
+++ Ex10-6.py
@@ -0,0 +1,8 @@
+"""
+You will get an error if you use double quotes inside a string that is surrounded by double quotes:
+txt = "We are the so-called "Vikings" from the north."
+"""
+
+# - - -
+
+txt = "We are the so-called \"Vikings\" from the north."
