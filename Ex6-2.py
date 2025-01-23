9997a87fee743b39b58564d544e56c469677c9b5
N_Sivkov <n_sivkov@kbtu.kz>
2025-01-23 11:54:59 +0500

    w3school examples from Python Variables

---
+++ Ex6-2.py
@@ -0,0 +1,27 @@
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+# - - -
+
+"""
+Illegal variable names:
+2myvar = "John"
+my-var = "John"
+my var = "John"
+"""
+
+# - - - Camel
+
+myVariableName = "John"
+
+# - - - Pascal
+
+MyVariableName = "John"
+
+# - - - Snake
+
+my_variable_name = "John"
---
+++ Ex6-4.py
@@ -0,0 +1,36 @@
+x = "Python is awesome"
+print(x)
+
+# - - -
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+# - - -
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+# - - -
+
+x = 5
+y = 10
+print(x + y)
+
+# - - -
+
+"""
+x = 5
+y = "John"
+print(x + y) -> here combining different datatypes this way will return an error
+"""
+
+# - - -
+
+x = 5
+y = "John"
+print(x, y)
